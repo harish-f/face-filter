d6e05217a3df419a5624fa3f1837a47169db51c2
Harish <harish@Murugas-MacBook-Air.local>
2024-02-16 12:15:41 +0800

    added slider for prop 1

--- app.py
+++ app.py
@@ -1,66 +1,8 @@
-# def overlay_image(frame, overlay_path, keypoint):
-#     # Load the overlay image
-#     image = cv2.imread(overlay_path, cv2.IMREAD_UNCHANGED)
-
-#     desired_width, desired_height = resize_overlay_image(True, image, 50)
-
-#     # Resize the image
-#     overlay_image = cv2.resize(image, (desired_width, desired_height))
-
-#     # Detect faces in the frame
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = detector(gray)
-
-#     # Check if at least one face is detected
-#     if len(faces) == 0:
-#         return frame
-
-#     # Iterate through detected faces
-#     for face in faces:
-#         # Detect facial keypoints
-#         landmarks = predictor(gray, face)
-
-#         # Convert dlib landmarks to numpy array
-#         landmarks_points = np.array([[landmark.x, landmark.y] for landmark in landmarks.parts()])
-
-#         # Extract the specified keypoint coordinates
-#         x, y = landmarks_points[keypoint]
-
-#         # Get the size of the overlay image
-#         overlay_height, overlay_width, _ = overlay_image.shape
-
-#         # Calculate the position to overlay the image
-#         x1 = int(x - overlay_width / 2)
-#         y1 = int(y - overlay_height / 2)
-#         x2 = int(x1 + overlay_width)
-#         y2 = int(y1 + overlay_height)
-
-#         # Ensure the overlay image fits within the frame boundaries
-#         x1 = max(0, x1)
-#         y1 = max(0, y1)
-#         x2 = min(frame.shape[1], x2)
-#         y2 = min(frame.shape[0], y2)
-
-#         # Resize the overlay image to fit the region of interest
-#         overlay_image_resized = cv2.resize(overlay_image, (x2 - x1, y2 - y1))
-
-#         # Overlay the resized image onto the frame
-#         alpha_s = overlay_image_resized[:, :, 3] / 255.0
-#         alpha_l = 1.0 - alpha_s
-#         for c in range(0, 3):
-#             frame[y1:y2, x1:x2, c] = (alpha_s * overlay_image_resized[:, :, c] +
-#                                        alpha_l * frame[y1:y2, x1:x2, c])
-
-#     return frame
-
 from flask import Flask, render_template, Response, request
 import cv2
 import dlib
 import numpy as np
 import mediapipe as mp
-
-
-
 
 app = Flask(__name__)
 
@@ -72,7 +14,7 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 
-prop_selected = 2
+prop_selected = 0
 
 def getFaceWidth(keypoints):
     return keypoints[16][0] - keypoints[0][0]
@@ -80,8 +22,11 @@ def getFaceWidth(keypoints):
 def getFaceHeight(keypoints):
     return keypoints[8][1] - keypoints[19][1]
 
-def isClicked(keypoints):
-    return ((keypoints[4].x - keypoints[8].x)**2 + (keypoints[4].y - keypoints[8].y)**2) ** 0.5 < 0.05
+def isClicked(keypoints): # using pythagoras theorom to determine the distance between thumb and index fingertip
+    thumb_tip = keypoints[4]
+    index_tip = keypoints[8]
+    dist = ((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2) ** 0.5 # returned as a float
+    return dist < 0.04
 
 def resize_overlay_image(keepDimension, image, width, height=0):
     if keepDimension and height == 0:
@@ -120,14 +65,13 @@ def overlay_image(frame, overlay_path, prop_index, scaleVal):
         landmarks_points = np.array([[landmark.x, landmark.y] for landmark in landmarks.parts()])
 
         if prop_index == 0:
-            desired_width, desired_height = resize_overlay_image(False, image, 150, 50)
+            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*scaleVal))
         elif prop_index == 1:
-            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*1.3))
+            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*1.3*scaleVal))
         elif prop_index == 2:
-            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*2))
+            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*2*scaleVal))
         elif prop_index == 3:
-            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*1))
-            # desired_width = int(desired_width * getFaceWidth(landmarks_points)/getFaceHeight(landmarks_points))
+            desired_width, desired_height = resize_overlay_image(True, image, int(getFaceWidth(landmarks_points)*1*scaleVal))
 
         # Resize the image
         overlay_image = cv2.resize(image, (desired_width, desired_height))
@@ -199,17 +143,17 @@ def video_feed():
     
     # Reading resolution of camera input
     _, frame = cap.read()
-    # frameWidth = len(frame[0])
-    # frameHeight = len(frame)
+    frameWidth = len(frame[0])
+    frameHeight = len(frame)
     
     
-    # clicked = False # initialise variable to check if finger is clicked
-    # initialFinger = 0 # initalise start position of finger gesture
-    # sliderVal = 0 # initialise slider value
-    # sliderWidth = 30 # specify width of slider
+    clicked = False # initialise variable to check if finger is clicked
+    initialFinger = 0 # initalise start position of finger gesture
+    sliderVal = 255 # initialise slider value 0 to 255
+    sliderSize = 30 # specify width of slider
 
-    # sliderBase = np.array([[255]*10]*(frameHeight-20)) # create slider base to be inserted into image
-    # sliderButton = np.array([[0]*30]*sliderWidth) # create slider shape to be inserted into image
+    sliderBase = np.array([[255]*10]*(frameHeight-20)) # create slider base to be inserted into image
+    sliderButton = np.array([[0]*25]*sliderSize) # create slider shape to be inserted into image
 
     # Load the overlay image
     overlay_path = ["assets/moustache.png", "assets/propellor_hat.png", "assets/cap_hat.png", "assets/glasses.png"]
@@ -221,41 +165,46 @@ def video_feed():
         if not ret:
             break
 
-        # Overlay the image with the specified keypoint
-        frame = overlay_image(frame, overlay_path[prop_selected], prop_selected, 0)
 
         # calculating the slider's position relative to the image
-        # buttonPos = int(sliderVal/265*frameHeight)
+        if sliderVal >= 255:
+            sliderVal = 254
+        elif sliderVal == 0:
+            sliderVal = 0
+        buttonPos = int(sliderVal/(sliderSize + 255)*frameHeight)
 
         # inserting slider module into the image
-        # for c in range(3):
-        #     frame[10:-10,-30:-20,c] = sliderBase
-        #     print(buttonPos, buttonPos+sliderWidth)
-        #     print(sliderButton.shape, frame[buttonPos:buttonPos+sliderWidth,-10:-40,c].shape)
-        #     frame[buttonPos:buttonPos+sliderWidth,-10:-40,c] = sliderButton
+        if prop_selected == 0:
+            for c in range(3):
+                frame[10:-10,-30:-20,c] = sliderBase
+                frame[buttonPos:buttonPos+sliderSize,-37:-12,c] = sliderButton
         
 
 
-        # with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-        #     frame = cv2.flip(frame, 1)
-        #     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #     detected_image = hands.process(image)
-        #     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        #     if detected_image.multi_hand_landmarks:
-        #         for hand_keypoints in detected_image.multi_hand_landmarks:
-        #             keypoints = hand_keypoints.landmark
-        #             print(clicked)
-        #             if clicked:
-        #                 sliderVal += (keypoints[4].y - initialFinger) * 265
-        #                 initialFinger = keypoints[4].y
-        #                 if not isClicked(keypoints):
-        #                     clicked = False
-        #                     initialFinger = 0
-        #             elif isClicked(keypoints):
-        #                 clicked = True
-        #                 initialFinger = keypoints[4].y
+        with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
             
+            # processing the image to get the hand keypoints
+            frame = cv2.flip(frame, 1)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            detected_image = hands.process(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if detected_image.multi_hand_landmarks:
+                for hand_keypoints in detected_image.multi_hand_landmarks: # Check all hands
+                    keypoints = hand_keypoints.landmark
+                    if clicked:
+                        if not isClicked(keypoints):
+                            clicked = False
+                            initialFinger = 0
+                            continue
+                        sliderVal += (keypoints[4].y - initialFinger) * (255 + sliderSize) * 2 # calculating change in sliderVal based on y movement of finger
+                        initialFinger = keypoints[4].y
+                    elif isClicked(keypoints): # changes clicked var to true if click is detected and clicked was initially false
+                        clicked = True
+                        initialFinger = keypoints[4].y
+        
+        # Overlay the image with the specified keypoint
+        frame = overlay_image(frame, overlay_path[prop_selected], prop_selected, (255/sliderVal)+0.5 if prop_selected == 0 else 1)
             
 
         # Encode the frame as JPEG
